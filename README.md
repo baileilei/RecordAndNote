f46aec3d3ad808f401cbe61a9cbd13a1a6ddd434
864029261 <Godot-SS.bai@aia.com>
2019-07-24 20:35:26 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # RecordAndNote
+
+1.swift
+2.push request
+3.朋友圈
+4.持久化
---
+++ RecordAndLocalNote/RecordAndLocalNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecordAndLocalNote
+//
+//  Created by g on 2019/7/24.
+//  Copyright © 2019 g. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
