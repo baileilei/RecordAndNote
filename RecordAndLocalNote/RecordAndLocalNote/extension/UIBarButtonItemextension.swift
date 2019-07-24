b7ebb69bafb0b59f4afca8bbba9aa0e00164a2f0
864029261 <Godot-SS.bai@aia.com>
2019-07-24 21:29:30 +0800

    UIBarbuttonItem  的便利构造函数 扩展！

---
+++ RecordAndLocalNote/RecordAndLocalNote/extension/UIBarButtonItemextension.swift
@@ -0,0 +1,30 @@
+//
+//  extension.swift
+//  RecordAndLocalNote
+//
+//  Created by g on 2019/7/24.
+//  Copyright © 2019 g. All rights reserved.
+//
+
+import UIKit
+
+extension UIBarButtonItem{
+    convenience init(_ title:String) {
+        self.init()
+        
+        let button = UIButton()
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(UIColor.darkGray, for: .normal)
+        button.setTitleColor(UIColor.orange, for: .highlighted)
+        
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        button.sizeToFit()
+        
+        customView = button
+    }
+}
+
+//class UIBarButtonItemextension: UIBarButtonItem {
+//
+//
+//}
