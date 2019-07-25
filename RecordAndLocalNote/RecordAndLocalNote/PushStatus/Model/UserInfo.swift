bed71c31d1063adbd66eb2f87cbd17072baaa22b
bwx738081 <baishangshang@huawei.com>
2019-07-25 16:25:33 +0800

    learn plist data write and read

---
+++ RecordAndLocalNote/RecordAndLocalNote/PushStatus/Model/UserInfo.swift
@@ -0,0 +1,13 @@
+//
+//  UserInfo.swift
+//  RecordAndLocalNote
+//
+//  Created by smart-wift on 2019/7/25.
+//  Copyright © 2019 g. All rights reserved.
+//
+
+import UIKit
+
+class UserInfo: NSObject {
+
+}
